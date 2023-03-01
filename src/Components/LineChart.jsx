dd0d14c0c6be525b211646c611077103a0ac49e5
dillibabu2003 <dillibabu.r2021csea@sece.ac.in>
2023-03-01 19:57:28 +0530

    After graph changes

--- src/Components/LineChart.jsx
+++ src/Components/LineChart.jsx
@@ -20,15 +20,39 @@ export default function Chart({ points }) {
             title: {
                 text: "Years",
             },
+            labels: {
+                step: 1
+            },
+            gridLineWidth: 1,
+            gridZIndex: 2,
         },
 
         yAxis: {
             enabled: false,
+            title: {
+                text: "",
+            },
+            labels: {
+                enabled: false,
+            },
+            gridLineColor: 'white',
         },
-        plotOptions:{
-            areaspline:{
-                color:'#00DD6F',
-            }
+
+        plotOptions: {
+            series: {
+                pointStart: 2023,
+                fillColor: {
+                    linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
+                    stops: [
+                        [0, 'rgba(0, 221, 111, 0.65)'],
+                        [1, 'rgba(204, 255, 230,0.2)']
+                    ]
+                }
+            },
+            areaspline: {
+                color: '#00DD6F',
+            },
+
         },
 
         tooltip: {
@@ -36,7 +60,7 @@ export default function Chart({ points }) {
             borderColor: '#FFFFFF',
             borderRadius: 20,
             style: {
-                color: '#0161FF',
+                color: '#000000',
             },
             formatter() {
                 return `Amount <strong>\u20B9 ${Number(this.y.toFixed(0)).toLocaleString("en-In")}</strong> <br> Year <strong> ${this.x} </strong>`
@@ -50,7 +74,7 @@ export default function Chart({ points }) {
         series: [
             {
                 name: 'Simple Interest',
-                data: [50000, 100000, 150000, 200000, 250000],
+                data: null,
             }
         ],
 
